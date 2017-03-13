55377b9c0fb671d82124c50f16e3b05878eab19b
AshMenhennett <ashleymenhennett@gmail.com>
2017-03-13 15:00:34 +1030

    Changed recommended browser to FireFox

--- resources/views/includes/partials/_upgrade_browser_danger.blade.php
+++ resources/views/includes/partials/_upgrade_browser_danger.blade.php
@@ -1,4 +1,4 @@
 <div class="upgrade-browser danger">
     <p>Your browser is outdated and does not support this application .</p>
-    <p>We suggest using <a href="https://www.google.com.au/chrome/browser" target="_blank" title="Google Chrome Browser">Google Chrome</a> for the best experience!</p>
+    <p>We suggest using <a href="https://www.mozilla.org/firefox/new/?scene=2" target="_blank" title="FireFox Browser">FireFox</a> for the best experience!</p>
 </div>
--- resources/views/includes/partials/_upgrade_browser_warning.blade.php
+++ resources/views/includes/partials/_upgrade_browser_warning.blade.php
@@ -1,3 +1,3 @@
 <div class="upgrade-browser warning">
-    <p>Try <a href="https://www.google.com.au/chrome/browser" target="_blank" title="Google Chrome Browser">Google Chrome Browser</a> for the best experience!</p>
+    <p>Try <a href="https://www.mozilla.org/firefox/new/?scene=2" target="_blank" title="FireFox Browser">FireFox Browser</a> for the best experience!</p>
 </div>
