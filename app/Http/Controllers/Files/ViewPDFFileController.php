0322563963943f6eac442dd474abdce3e8df3a2d
AshMenhennett <ashleymenhennett@gmail.com>
2017-03-30 21:22:56 +1030

    Fixed namespace of ViewPDFFileController

--- app/Http/Controllers/Files/ViewPDFFileController.php
+++ app/Http/Controllers/Files/ViewPDFFileController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Files;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ViewPDFFileController extends Controller
 {
